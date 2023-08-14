c1b46c0e5042a402c7a4011a8f303f399aab53c8
Lambari <78892876+AndreiBoeck@users.noreply.github.com>
2023-08-13 22:22:28 -0300

    Fixing some errors

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,76 +1,80 @@
 class CaixaDaLanchonete {
-
     calcularValorDaCompra(metodoDePagamento, itens) {
-        let valorTotal = selectItens(itens);
+        const valorTotal = this.selectItens(itens); // Fixed: Call selectItens using 'this'
+        
+        if (typeof valorTotal === 'string') {
+            return valorTotal; // Return error messages directly
+        }
+
         switch (metodoDePagamento) {
             case "dinheiro":
-                valorTotal -= (valorTotal * 0.05);
+                valorTotal -= valorTotal * 0.05;
                 break;
             case "credito":
-                valorTotal += (valorTotal * 0.03);
+                valorTotal += valorTotal * 0.03;
                 break;
             case "debito":
                 break;
             default:
                 return "Forma de pagamento inválida!";
-                break;
-        }       
+        }
+
         return [metodoDePagamento, itens, valorTotal];
     }
-    selectItens(itens){
+
+    selectItens(itens) {
         let valueItens = 0;
-        if(itens.length == 0){
+
+        if (itens.length === 0) {
             return "Não há itens no carrinho de compra!";
         }
+
         for (let i = 0; i < itens.length; i++) {
-            if(itens[i].slice(-1) == 0){
+            if (itens[i].slice(-1) == 0) {
                 return "Quantidade inválida!";
             }
-            switch (itens[i]) {     //Calculo de valores de cada item
+
+            const itemName = itens[i].slice(0, -2); // Extract item name
+            const itemQuantity = parseInt(itens[i].slice(-1)); // Extract item quantity
+
+            switch (itemName) {
                 case "cafe":
-                    valueItens += 3.00 * itens[i].slice(-1);
+                    valueItens += 3.00 * itemQuantity; 
                     break;
                 case "chantily":
-                    if (itens.include("cafe")) {
-                        valueItens += 1.50 * itens[i].slice(-1);
-                    }
-                    else{
+                    if (itens.includes("cafe")) {
+                        valueItens += 1.50 * itemQuantity; 
+                    } else {
                         return "Item extra não pode ser pedido sem o principal";
                     }
                     break;
-                    break;
                 case "suco":
-                    valueItens += 6.20 * itens[i].slice(-1);
+                    valueItens += 6.20 * itemQuantity; 
                     break;
                 case "sanduiche":
-                    valueItens += 6.50 * itens[i].slice(-1);
+                    valueItens += 6.50 * itemQuantity; 
                     break;
                 case "queijo":
-                    if(itens.include("sanduiche")){
-                        valueItens += 2.00 * itens[i].slice(-1);
-                    }
-                    else{
+                    if (itens.includes("sanduiche")) {
+                        valueItens += 2.00 * itemQuantity; 
+                    } else {
                         return "Item extra não pode ser pedido sem o principal";
                     }
                     break;
                 case "salgado":
-                    valueItens += 7.25 * itens[i].slice(-1);
+                    valueItens += 7.25 * itemQuantity; 
                     break;
                 case "combo 1":
-                    valueItens += 9.50 * itens[i].slice(-1);
+                    valueItens += 9.50 * itemQuantity; 
                     break;
                 case "combo 2":
-                    valueItens += 7.50 * itens[i].slice(-1);
-                    break;        
+                    valueItens += 7.50 * itemQuantity; 
+                    break;
                 default:
                     return "Item inválido!";
-                    break;
             }
-            
         }
+
         return valueItens;
     }
-
 }
-
-export { CaixaDaLanchonete };
