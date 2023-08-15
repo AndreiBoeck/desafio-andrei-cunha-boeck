7aa798821ca21e428ad2c62c30ff0209d35f1e50
Andrei <andreicunhaboeck@gmail.com>
2023-08-14 22:53:35 -0300

    Bugfix when more than 10 units

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,4 +1,5 @@
 class CaixaDaLanchonete {
+    
     calcularValorDaCompra(metodoDePagamento, itens) {
         
         let valorTotal = (this.selectItens(itens)); // Fixed: Call selectItens using 'this'
@@ -34,18 +35,20 @@ class CaixaDaLanchonete {
         }
 
         for (let i = 0; i < itens.length; i++) {
-            if(itens[i].slice(0, -2) == 'cafe' || itens[i].slice(0, -2) == 'sanduiche') {
-                validate.push(itens[i].slice(0, -2));
+            let comma = itens[i].indexOf(",");
+            if(itens[i].slice(0, (comma * -1)) == 'cafe' || itens[i].slice(0, comma) == 'sanduiche') {
+                validate.push(itens[i].slice(0, comma));
             }
         }
 
         for (let i = 0; i < itens.length; i++) {
-            if (itens[i].slice(-1) == 0) {      //dettect ilegal quantity and return error
+            let comma = itens[i].indexOf(",");
+            if (itens[i].slice(comma + 1) == 0) {      //dettect ilegal quantity and return error
                 return 'Quantidade inválida!';
             }
 
-            let itemName = (itens[i].split(" ").join("")).slice(0, -2); // Extract item name and standardizes itens
-            let itemQuantity = parseInt(itens[i].slice(-1)); // Extract item quantity
+            let itemName = (itens[i].split(" ").join("")).slice(0, comma); // Extract item name and standardizes itens
+            let itemQuantity = parseInt(itens[i].slice(comma + 1)); // Extract item quantity
 
             switch (itemName) {     //Select prices by label
                 case 'cafe':
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { CaixaDaLanchonete } from "./caixa-da-lanchonete.js";
+
+let teste = new CaixaDaLanchonete().calcularValorDaCompra('debito', ['cafe,72']);
+    console.log(teste);
